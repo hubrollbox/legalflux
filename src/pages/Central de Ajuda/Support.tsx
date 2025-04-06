9655a6a308f37250db5fc18f80aebf0c18769c1b
legalflux <suporte@legalflux.pt>
2025-04-06 23:38:09 +0100

    feat: add Support page component for help and resources

---
+++ src/pages/Central de Ajuda/Support.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Support = () => {
+  return (
+    <div>
+      <h1>Support Page</h1>
+      <p>Welcome to the Support page. Here you can find help and resources.</p>
+    </div>
+  );
+};
+
+export default Support;
