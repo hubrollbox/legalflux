615788b188c8c0fcf67ef4419b86e04b693d03bf
legalflux <suporte@legalflux.pt>
2025-04-23 15:53:22 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      "plugin:@next/next/recommended"
+      ...nextPlugin.configs.recommended
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
