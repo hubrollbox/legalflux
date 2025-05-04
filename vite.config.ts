c2c48a018b9d60274dc96641bd6b04579017981e
legalflux <suporte@legalflux.pt>
2025-05-04 21:00:52 +0100

    ci: increase maximum file size for caching in workbox configuration
    The maximum file size for caching in the workbox configuration was increased to 3MB to accommodate larger assets that were previously not being cached, improving the performance of the application by ensuring these assets are served from the cache.

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,9 @@ export default defineConfig(({ mode }) => ({
           },
         ],
       },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3000000
+      },
     }),
   ].filter(Boolean),
   resolve: {
