ba742ae7b019b893350a5b1c09c469982dfd67e6
legalflux <suporte@legalflux.pt>
2025-05-04 17:50:40 +0100

    refactor(documents): move formatDate utility to documentCardUtils
    This change improves code organization by relocating the formatDate function to a dedicated utility file, enhancing maintainability and reusability.

--- src/components/documents/TemplateList.tsx
+++ src/components/documents/TemplateList.tsx
@@ -11,7 +11,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { formatDate } from "./DocumentCard";
+import { formatDate } from "./documentCardUtils";
 
 interface TemplateListProps {
   templates: Array<{
